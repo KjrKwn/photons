751d1076945e80af8a70e2ed800b7bc5663e0c67
KAWANAKojiro-no-MacBook-Pro <kawana@utap.phys.s.u-tokyo.ac.jp>
2019-04-19 13:32:30 +0900

    add description

--- analysis/python/photontools/__init__.py
+++ analysis/python/photontools/__init__.py
@@ -1,6 +1,15 @@
 """
 Tools to analyze outputs of radiative transfer calcultions
 developed by Kojiro Kawana 190419
+
+Work in python 3
+
+Module requirement
+==================
+numpy
+pandas
+astropy
+
 """
 
 from photontools.photontools import Spectra, Lightcurve, Filter, read_Maeda_data, read_MacLeod_data, read_all_filters, read_one_instrument_filters, calc_band_flux
